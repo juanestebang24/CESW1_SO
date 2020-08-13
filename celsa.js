cae12694ce09cf5fd3ab86477d9bb4bb5665c632
juanestebang24 <66138435+juanestebang24@users.noreply.github.com>
2020-08-13 10:54:52 -0500

    semana 9

---
+++ celsa.js
@@ -0,0 +1,12 @@
+'uso estricto'
+
+const  multiplicacion  =  ( num1 , num2 )  =>  {
+
+    return  num1 * num2 ;
+
+}
+
+
+consola . log ( multiplicacion ( 4 , 8 ) ) ;
+consola . log ( multiplicacion ( 15 , 3 ) ) ;
+consola . log ( multiplicacion ( 98 , 2 ) ) ;
